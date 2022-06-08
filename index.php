8cef4d76c15a0eed64c34903a1d0e081b44167ba
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-06-08 16:47:20 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!-- Riscrivere questa pagina del sito google
+https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte.
+Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP.
+Per la formattazione del testo della FAQ inserite il markup HTML direttamente nel “database”
+buon lavoro! -->
+<?php
+$dblinks = [
+
+];
+
+$dbfaqs = [
+
+];
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Domande frequenti - Privaci e termini - Google</title>
+    <link rel="shortcut icon" href="./img/google-flat.png" type="image/x-icon">
+</head>
+<body>
+    
+</body>
+</html>
